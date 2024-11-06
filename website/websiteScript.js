45ef46445b782f7e773e7b7e1f50da9f8595c69d
sulit <estorninoadrian0720@gmail.com>
2024-11-06 13:34:56 +0800

    add folder directory

--- website/websiteScript.js
+++ website/websiteScript.js
@@ -1,11 +1,11 @@
 // Handle Sign Up button click
 document.getElementById('signupBtn').addEventListener('click', () => {
-    window.location.href = 'signuplogin.html'; // Redirect to sign-up/login page
+    window.location.href = '/signuplogin.html'; // Redirect to sign-up/login page
 });
 
 // Handle Sign In button click
 document.getElementById('loginBtn').addEventListener('click', () => {
-    window.location.href = 'signuplogin.html'; // Redirect to sign-up/login page
+    window.location.href = '/signuplogin.html'; // Redirect to sign-up/login page
 });
 
 
