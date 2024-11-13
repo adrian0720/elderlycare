4479578d62ae1d82beb957631b359b9b6c6fe20f
Elderly Care <168039452+adrian0720@users.noreply.github.com>
2024-11-14 06:56:59 +0800

    Update websitescript.js

--- websitescript.js
+++ websitescript.js
@@ -1,11 +1,11 @@
 // Handle Sign Up button click
 document.getElementById('signupBtn').addEventListener('click', () => {
-    window.location.href = 'signuplogin.html'; // Redirect to sign-up/login page
+    window.location.href = '/signuplogin.html'; // Redirect to sign-up/login page
 });
 
 // Handle Sign Up button click
 document.getElementById('signupBtn').addEventListener('click', () => {
-    window.location.href = 'signuplogin.html'; // Redirect to sign-up/login page
+    window.location.href = '/signuplogin.html'; // Redirect to sign-up/login page
 });
 
 
